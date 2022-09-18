302fa1e42332b61b2097d73d4743fddbfc55bb9a
Gimena Bistolfi <bistolfigimena@gmail.com>
2022-09-18 19:53:59 -0300

    BaSP-W04 First exercise complete

---
+++ Semana 04/exercises/01_variables_and_operators.js
@@ -0,0 +1,32 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+//a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('Exercise 1.a:');
+
+var edad = 20;
+var peso = 60;
+
+var sumaDeLosNumeros = edad + peso;
+console.log(sumaDeLosNumeros);
+
+
+//b.Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('Exercise 1.b:');
+
+var uno = "Bienvenida";
+var dos = "Gimena";
+
+var bienvenidaGimena = (uno + " " + dos);
+console.log(bienvenidaGimena);
+
+
+//c.Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log('Exercise 1.c:');
+
+var one = "Me llamo"
+var two = "Gimena Bistolfi"
+
+console.log(one.length)
+console.log(two.length)
