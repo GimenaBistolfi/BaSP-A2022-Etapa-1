59b3735d36869815e373e2161479301575e43318
Gimena Bistolfi <bistolfigimena@gmail.com>
2022-09-20 22:06:50 -0300

    BaSP-W04 Last Exercise Complete 6_Functions.

--- Semana 04/exercises/06_functions.js
+++ Semana 04/exercises/06_functions.js
@@ -8,11 +8,11 @@ console.log('Exercise 6.a:');
 var numberOne = 15
 var numberTwo = 15
 
-function suma(_num1,_num2){
+function sum(_num1,_num2){
     return _num1 + _num2;
 }
 
-var endFigure = suma(numberOne,numberTwo);
+var endFigure = sum(numberOne,numberTwo);
 
 console.log('Value 1: ' + numberOne);
 console.log('Value 2: ' + numberTwo);
@@ -36,14 +36,14 @@ function addition(_num1, _num2, _message){
   
   var numberThree = 15;
   var numberFour = 'g';
-  var message1 = 'error: los valores deben ser numéricos';
+  var message1 = 'error: values must be numeric';
   var addition1 = addition(numberThree,numberFour,message1);
   
   console.log('Value 1: ' + numberThree);
   console.log('Value 2: ' + numberFour);
   console.log('Addition: ' + addition1);
 
-  alert('error: los valores deben ser numericos');
+  alert('error: values must be numeric');
 
 //c. Aparte, crear una función validate Integer que reciba un número como parámetro y devuelva verdadero
 // si es un número entero.
@@ -70,27 +70,36 @@ function validateInteger(_val){
 
 console.log('Exercise 6.d:');
 
-function addition2(_num1, _num2, _message){  
-    if (validateInteger(_num1) && validateInteger(_num2)) {
-      return _num1 + _num2;
-    } else {
-      return _message + parseInt(_num1) + ' ' + parseInt(_num2);
+function theNumbers6d (numberOne,numberTwo){
+    if (validateInteger (numberOne) && validateInteger(numberTwo)) {
+        return numberOne + numberTwo;
+    }else {
+        return alert('Uno de los parametros tiene un error'), (Math.round(numberOne) + " " + Math.round(numberTwo));
+
     }
-  }
-  
-  var numberSix = 20;
-  var numberSeven = 'h';
-  var message2 = 'error: los valores deben ser enteros. ';
-  var addition2 = addition3(numberSix,numberSeven,message2);
-  
-  console.log('Value 1: ' + numberSix);
-  console.log('Value 2: ' + numberSeven);
-  console.log('Addition: ' + addition2);
+}
+
+console.log(theNumbers6d (10,10));
+
 
 
 //e. Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de la función suma 
 //probando que todo siga funcionando igual.
 
 console.log('Exercise 6.e:');
+
+function becomeInteger(_val){
+    if (!Number.isInteger(_val)){
+      return parseInt(_val);
+    }
+  }
+  
+  function addition3(_num1, _num2){  
+    if (validateInteger(_num1) && validateInteger(_num2)) {
+      return alert(becomeInteger(_num1) + becomeInteger(_num2));
+    } else {
+      return alert('values must be integer' + Math.round(_num1) + ' ' + Math.round(_num2));
+    }
+  }
 
 
